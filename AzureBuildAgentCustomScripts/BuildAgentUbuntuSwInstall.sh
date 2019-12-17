febfacfbea6cc6a47e917d842f1d246208fc8a77
MiRchandrika <58923734+MiRchandrika@users.noreply.github.com>
2019-12-18 10:32:18 +1300

    Update BuildAgentUbuntuSwInstall.sh

--- AzureBuildAgentCustomScripts/BuildAgentUbuntuSwInstall.sh
+++ AzureBuildAgentCustomScripts/BuildAgentUbuntuSwInstall.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# BuildAgentUbuntuSwInstall.sh
+# This script is used to install required software on a ubuntu Azure Build Agent.
+# This script will be in a public accessable location like Azure Storage and
+# its url will be used in the Azure ARM template VM Extension resource to deploy on the new VM.
+# This script will be downloaded on to `/var/lib/waagent/custom-script/download/0/` path on the VM.
+# The Azure script extenstion produces a log at `/var/log/azure/custom-script/handler.log`
+# To troubleshoot, first check the Linux Agent Log, ensure the extension ran, at /var/log/waagent.log
+# For more info on how this works, visit https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux
+
+# The parameters that it takes is 
+# Azure Build Agent username as $1 - required parameter
+# Azure Auth PAT token as $2 - required parameter
+# Azure Agent pool name as $3 - required parameter
+# Azure Build Agent name as $4 - optional, defaulted to the hostname
+# Azure VSTS Account name as $5 - optional, defaulted to 'telstradx'
+
 #-------------------------------- Azure Build Agent ------------------------------------------
 
 AzureAgentUserName=$1
@@ -25,7 +41,11 @@ su - $AzureAgentUserName -c "tar zxvf $AzureBuildAgentHome/vsts-agent-linux-x64-
 sudo chown $AzureAgentUserName:$AzureAgentUserName -R $AzureBuildAgentHome
 su - $AzureAgentUserName -c "bash $AzureBuildAgentHome/config.sh --unattended --url https://dev.azure.com/$AzureVSTSAccount --auth pat --token $AzureAuthPatToken --pool $AzureAgentPool --agent $AzureBuildAgent --work usr/local/agent_work"
 su - $AzureAgentUserName -c "nohup bash $AzureBuildAgentHome/run.sh > /dev/null 2>&1 &"
-echo "Linux Build Agent started running"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed Azure Linux Build Agent"
+else
+  echo "ERROR: Something went wrong while installing Azure Linux Build Agent"
+fi 
 
 #------------------------------------ Azure CLI -----------------------------------------------
 
@@ -40,7 +60,11 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
 sudo apt-get install -y azure-cli
-echo "Installed Azure CLI"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed Azure CLI"
+else
+  echo "ERROR: Something went wrong while installing Azure CLI"
+fi 
 
 #-------------------------------- Docker Engine CE ----------------------------------------------
 
@@ -60,7 +84,11 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce=5:19.03.5~3-0~ubuntu-xenial docker-ce-cli=5:19.03.5~3-0~ubuntu-xenial containerd.io
-echo "Installed the Docker Engine CE"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed Docker Engine CE"
+else
+  echo "ERROR: Something went wrong while installing Docker Engine CE"
+fi 
 
 #----------------------------------------- Kubectl -------------------------------------------------
 
@@ -70,7 +98,11 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-echo "Installed kubectl"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed kubectl"
+else
+  echo "ERROR: Something went wrong while installing kubectl"
+fi 
 
 #-------------------------------------------- sbt ---------------------------------------------------
 
@@ -80,7 +112,11 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.li
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
 sudo apt-get update
 sudo apt-get install -y sbt
-echo "Installed sbt"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed sbt"
+else
+  echo "ERROR: Something went wrong while installing sbt"
+fi 
 
 #-------------------------------- .NET Core Runtime and SDK ------------------------------------------
 
@@ -95,7 +131,11 @@ sudo apt-get update
 sudo apt-get install -y dotnet-runtime-2.2
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-2.2
-echo "Installed .NET Core SDK and Runtime"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed .NET core SDK and Runtime"
+else
+  echo "ERROR: Something went wrong while installing .NET core SDK and Runtime"
+fi 
 
 #---------------------------------------- Nodejs and NPM -----------------------------------------------
 
@@ -104,13 +144,21 @@ sudo apt-get install -y gcc g++ make
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get update
 sudo apt install -y nodejs
-echo "Installed nodejs and npm"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed nodejs and npm"
+else
+  echo "ERROR: Something went wrong while installing nodejs and npm"
+fi 
 
 #------------------------------------------- Kubeseal ---------------------------------------------------
 
 echo "Installing Kubeseal Client"
 wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.9.6/kubeseal-linux-amd64 -O kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
-echo "Installed Kubeseal client"
+if [[ $? -eq 0 ]]; then
+  echo "Successfully Installed Kubeseal client"
+else
+  echo "ERROR: Something went wrong while installing Kubeseal client"
+fi 
 
 #------------------------------------------- end of script -----------------------------------------------
